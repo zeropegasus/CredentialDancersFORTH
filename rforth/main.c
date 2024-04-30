8d350935f5a590ce6fd94bc40afda4f9912f3c00
nathan ferrell <nferrell6@gmail.com>
2024-04-30 18:50:16 +0000

    forgot one line

--- rforth/main.c
+++ rforth/main.c
@@ -247,6 +247,8 @@ void execute_command(const char* command, int_stack_t *stack) {
                 int_stack_swap(stack);
             } else if (strcmp(command, "over") == 0) {
                 int_stack_over(stack);
+            } else if (strcmp(actual_command, "man") == 0) {
+                int_stack_man();
             } else if (strcmp(command, "rot") == 0) {
                 int_stack_rot(stack);
             } else if (strcmp(command, "drop") == 0) {
